4c1fe98170a8c30789c8efdcf104786429bd8883
JesseUgljesic <159741403+JesseUgljesic@users.noreply.github.com>
2025-02-14 10:21:19 +1100

    sprint and stamina

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
-var SPEED = 20.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
