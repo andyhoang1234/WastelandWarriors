74c477b09034a8121efc2abeb8766fb0ecbde2bb
Tashi <8030071@schoolsnet.act.edu.au>
2025-03-24 13:16:58 +1100

    enemy updated

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
 var SPEED = 0
-
+var enemy_health = 100
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -30,5 +30,15 @@ func _physics_process(_delta):
 
 	# Vector Maths
 	var new_velocity = (next_location - current_location).normalized() * SPEED
+	
 	velocity = new_velocity
+	
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		#print("I collided with ", collision.get_collider().name)
+		if collision.get_collider().is_in_group("Bullet.tscn"):
+			reduce_health(100)
+
+func reduce_health(amount):
+	enemy_health -= amount
