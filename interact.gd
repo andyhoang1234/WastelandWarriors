50785b57eb332c7f4d2e5387f61fe5b10cfb76d5
JesseUgljesic <159741403+JesseUgljesic@users.noreply.github.com>
2025-05-09 10:35:31 +1000

    Interact some

--- interact.gd
+++ interact.gd
@@ -1,15 +1,58 @@
 extends RayCast3D
 
+var interact_cast_result
+var current_cast_result
+var interact_distance := 10.0  # Set a default interaction distance
 var interactable = true
 @export var toggle: bool = false
 @export var open_position: Vector3 = Vector3(0, 0, -2) # Adjust this based on your scene
 @export var closed_position: Vector3 = Vector3(0, 0, 0) # Default position
 @onready var door: Node3D = $Door
 
+@onready var camera = get_node("/root/testWorld/1/Player/Camera3D")
 
+func _input(event):  
+	if event.is_action_pressed("interact"):
+		interact()
+
+func interact_cast() -> void:
+	print("interact cast")
+	var space_state = camera.get_world_3d().direct_space_state
+	var screen_center = get_viewport().size / 2
+	var origin = camera.project_ray_origin(screen_center)
+	var end = origin + camera.project_ray_normal(screen_center) * interact_distance
+
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_bodies = true
+	query.set_exclude([self])
+
+	var result = space_state.intersect_ray(query)
+	current_cast_result = result.get("collider")
+
+	if current_cast_result != interact_cast_result:
+		if interact_cast_result and interact_cast_result.has_user_signal("unfocused"):
+			interact_cast_result.emit_signal("unfocused")
+
+		interact_cast_result = current_cast_result
+
+		if interact_cast_result and interact_cast_result.has_user_signal("focused"):
+			interact_cast_result.emit_signal("focused")
+
+func interact() -> void:
+	if interact_cast_result:
+		print("cast result")
+		if interact_cast_result.has_user_signal("interacted"):
+			print("interact")
+			interact_cast_result.emit_signal("interacted")
+
+func _ready() -> void:
+	pass
+	
 func _physics_process(delta):
 	pass
 
+func _process(delta: float) -> void:
+	pass
 			
 func move_door(state: bool):
 	var target_position = open_position if state else closed_position
--- models/door.gd
+++ models/door.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
+
 # --- Parameters and state ---
 var interactable = true
 @export var toggle: bool = false
-@export var open_position: Vector3 = Vector3(0, 0, -2)
-@export var closed_position: Vector3 = Vector3(0, 0, 0)
+@export var open_position: Vector3 = Vector3(0, 0, -2) # Adjust this based on your scene
+@export var closed_position: Vector3 = Vector3(0, 0, 0) # Default position
 
 @onready var door: Node3D = $Door
 var is_open := false  # Track door state
@@ -12,6 +13,8 @@ var is_open := false  # Track door state
 # --- Declare the signal ---
 signal interacted
 
+func _physics_process(delta):
+	pass
 func _ready():
 	# Connect the interacted signal to self to handle interaction
 	connect("interacted", _on_interacted)
@@ -24,7 +27,7 @@ func _on_interacted():
 		else:
 			move_door(true)
 
+			
 func move_door(state: bool):
 	var target_position = open_position if state else closed_position
 	var tween = create_tween()
-	tween.tween_property(door, "position", target_position, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
