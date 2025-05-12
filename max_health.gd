ede3360924c022d11d1def5c7fc2623148fb2011
TariG213 <0676776@schoolsnet.act.edu.au>
2025-05-12 12:39:57 +1000

    Fixes

--- max_health.gd
+++ max_health.gd
@@ -1,20 +1,11 @@
-extends Node3D
+extends Area3D
 
-var health_max = 100
-var health_increase = 100  # This can be set to the player's max health
+signal max_health_powerup_collected
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var health_increase_amount : int = 20
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-
-func _on_rigid_body_3d_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):  # Ensure the player picks up the power-up
-		body.increase_max_health()  # Call the method to restore health
-		queue_free()  # Remove the power-up after pickup
-		print("JESSE")
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("player"):
+		area.increase_max_health(health_increase_amount)
+		emit_signal("max_health_powerup_collected")
+		queue_free()
