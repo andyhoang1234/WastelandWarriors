d8da5fd72664638ac5ce889df8fc39615cf8a1c2
TariG213 <0676776@schoolsnet.act.edu.au>
2025-05-19 12:45:21 +1000

    Made dorrah

--- Global.gd
+++ Global.gd
@@ -5,6 +5,7 @@ var aiming
 var HMult:float
 var instakill
 var health
+var dorrah
 
 # if status is true, increases the size of the window and scales on-screen elements to window size
 # allows for better viewing on retina (or similar resolution) displays
@@ -18,3 +19,5 @@ func high_resolution_display_mode(status):
 func _ready():
 	high_resolution_display_mode(true)
 	HMult == 0
+
+	dorrah == 0 
