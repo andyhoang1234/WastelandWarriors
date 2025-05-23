a7ed081012a707e4904e5f9d41e48decd31bad26
MaxDries <159208443+MaxDries@users.noreply.github.com>
2025-05-23 10:31:13 +1000

    MONEY on hud

--- label.gd
+++ label.gd
@@ -7,10 +7,10 @@ func _ready():
 func update_label():
 	self.text = "dorrah: " + str(Global.dorrah)
 
-func add_money(amount):
-	Global.add_money(amount)
+func add_dorrah(amount: int):
+	Global.add_dorrah(amount)
 	update_label()
 
-func subtract_money(amount):
+func subtract_money(amount: int):
 	Global.subtract_money(amount)
 	update_label()
