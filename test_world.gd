00f874cc53e1f00dfb9efb5123a46683167a1ffd
Ryan Cather <ryan.cather@ed.act.edu.au>
2025-06-27 13:49:04 +1000

    multiplayer

--- test_world.gd
+++ test_world.gd
@@ -1,5 +1,10 @@
 extends Node
 
+
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
+
 @onready var main_menu = $CanvasLayer/MainMenu
 @onready var address_entry = get_node_or_null("CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry")
 @onready var hud = $CanvasLayer/HUD
@@ -107,3 +112,40 @@ func _on_menu_button_pressed() -> void:
 	main_menu.show()
 	Lose.hide()
 	hud.hide()
+	
+
+
+func _on_host_button_pressed():
+	main_menu.hide()
+	hud.show()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	add_player(multiplayer.get_unique_id())
+	
+	#upnp_setup()
+func _on_join_button_pressed():
+	main_menu.hide()
+	hud.show()
+	
+	enet_peer.create_client(address_entry.text, PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.health_changed.connect(update_health_bar)
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover Failed! Error %s" % discover_result)
+
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway!")
+
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
